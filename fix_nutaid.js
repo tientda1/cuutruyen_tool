ddbd980f00a4d806e6a6cfe4a56cc16d4f2646bd
tientda1 <shadowsgamer371@gmail.com>
2026-06-09 13:12:33 +0700

    chore: remove junk and test files

--- fix_nutaid.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/nutaid.js', 'utf8');
-content = content.replace(/\\\`/g, '`').replace(/\\\$/g, '$');
-fs.writeFileSync('src/nutaid.js', content);
